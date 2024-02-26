c273c858f837d9fafc7b47c1b0cd0cd3eb06ee37
Damien Bolger <damienbolger@gmail.com>
2024-02-26 14:00:24 +1300

    add -y

--- platform/confighooks/prebuild/01_setup_environment.sh
+++ platform/confighooks/prebuild/01_setup_environment.sh
@@ -2,8 +2,8 @@
 
 echo "build php extensions"
 
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum install yum-utils -y
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum install -y yum-utils
 yum-config-manager --enable remi-php81
 yum install -y php-cli php-imagick php-mbstring php-pear php-devel php-imagick php-opcache php-intl php-mysqlnd php-gd php-exif php-xsl php-bcmath php-mysqli php-sodium php-mailparse
