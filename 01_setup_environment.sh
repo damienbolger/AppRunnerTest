ede59c8a76bbee00182a68323f20046ce8326a71
Damien Bolger <damienbolger@gmail.com>
2024-02-26 15:34:45 +1300

    asdf

--- 01_setup_environment.sh
+++ 01_setup_environment.sh
@@ -2,10 +2,10 @@
 
 echo "build php extensions"
 
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum install -y yum-utils
-yum-config-manager --enable remi-php81
-yum install -y php-cli php-imagick php-mbstring php-pear php-devel php-imagick php-opcache php-intl php-mysqlnd php-gd php-exif php-xsl php-bcmath php-mysqli php-sodium php-mailparse
+# yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+# yum install -y yum-utils
+# yum-config-manager --enable remi-php81
+# yum install -y php-cli php-imagick php-mbstring php-pear php-devel php-imagick php-opcache php-intl php-mysqlnd php-gd php-exif php-xsl php-bcmath php-mysqli php-sodium php-mailparse
 
 
