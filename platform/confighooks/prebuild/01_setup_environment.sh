460a21c3d6498d3667dc25090596b1ec457ba4ed
Damien Bolger <damienbolger@gmail.com>
2024-02-26 13:42:30 +1300

    remove

--- platform/confighooks/prebuild/01_setup_environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Build php extensions"
-
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum install yum-utils -y
-yum-config-manager --enable remi-php81
-yum install -y php-cli php-imagick php-mbstring php-pear php-devel php-imagick php-opcache php-intl php-mysqlnd php-gd php-exif php-xsl php-bcmath php-mysqli php-sodium php-mailparse
