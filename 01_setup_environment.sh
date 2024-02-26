a96a481451050554fa6b2b7b28cb4be2cd496861
Damien Bolger <damienbolger@gmail.com>
2024-02-27 10:33:35 +1300

    asdf

--- 01_setup_environment.sh
+++ 01_setup_environment.sh
@@ -2,9 +2,9 @@
 
 # yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 # yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum -y update
+# yum -y update
 # sudo yum install -y yum-utils
 # yum-config-manager --enable remi-php81
 yum install -y php-cli
 # sudo yum install -y php-cli php-imagick php-mbstring php-pear php-devel php-imagick php-opcache php-intl php-mysqlnd php-gd php-exif php-xsl php-bcmath php-mysqli php-sodium php-mailparse
-chmod +x startup.sh
+chmod +x ./startup.sh
