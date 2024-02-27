d33632a9105d3d9904fea3276edc66abcc07f159
Damien Bolger <damienbolger@gmail.com>
2024-02-27 14:32:54 +1300

    asdf

--- 01_setup_environment.sh
+++ 01_setup_environment.sh
@@ -6,5 +6,3 @@ yum -y update
 # yum install -y yum-utils
 # yum-config-manager --enable remi-php81
 yum install -y php-cli php-imagick php-mbstring php-pear php-devel php-imagick php-opcache php-intl php-mysqlnd php-gd php-exif php-xsl php-bcmath php-mysqli php-sodium php-mailparse
-which yum
-ls -lah
