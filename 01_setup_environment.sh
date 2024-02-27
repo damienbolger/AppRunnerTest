aa049b49b1302242cfdbfc15f8b0c3f22afa42db
Damien Bolger <damienbolger@gmail.com>
2024-02-27 15:08:51 +1300

    asdf

--- 01_setup_environment.sh
+++ 01_setup_environment.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-# yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+
 echo "update yum"
 yum -y update
-# yum install -y yum-utils
-# yum-config-manager --enable remi-php81
 
 echo "Install php extensions"
-yum install -y php-cli php-imagick php-mbstring php-pear php-devel php-imagick php-opcache php-intl php-mysqlnd php-gd php-exif php-xsl php-bcmath php-mysqli php-sodium php-mailparse
+yum install -y php-cli php-mbstring php-pear php-devel php-opcache php-intl php-mysqlnd php-gd php-exif php-xsl php-bcmath php-mysqli php-sodium php-mailparse
 php --ini
+
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum install -y yum-utils
+yum-config-manager --enable remi-php81
+yum install -y php-imagick
+php --ri imagick
