789a8e847a55093ce4e2a92de67a4d98872059de
Damien Bolger <damienbolger@gmail.com>
2024-02-27 16:06:40 +1300

    asdf

--- 01_setup_environment.sh
+++ 01_setup_environment.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
 
-echo "disable yum priorities"
+echo "disable yum priorities ****************************"
 echo -e "[main]\nenabled=0" | tee /etc/yum/pluginconf.d/priorities.conf
 echo "check yum priorities"
 cat /etc/yum/pluginconf.d/priorities.conf
 
-exit
 
-echo "update yum"
-yum -y update
-
-echo "INSTALL PHP EXTENSIONS"
-yum install -y php-cli php-mbstring php-pear php-devel php-opcache php-intl php-mysqlnd php-gd php-exif php-xsl php-bcmath php-mysqli php-mailparse
-php --ini
-
-echo "INSTALL EPEL AND REMI REPOS"
+echo "INSTALL EPEL AND REMI REPOS ****************************"
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+echo "update yum and install utils ****************************"
+yum -y update
 yum install -y yum-utils
+
 yum-config-manager --disable 'remi-php*'
 yum-config-manager --enable remi-php81
-yum repolist
+
+echo "INSTALL REGULAR PHP EXTENSIONS ****************************"
+yum install -y php-cli php-mbstring php-pear php-devel php-opcache php-intl php-mysqlnd php-gd php-exif php-xsl php-bcmath php-mysqli php-mailparse
+
 echo "INSTALL IMAGICK ****************************"
 yum install -y php-imagick
 yum install -y php-sodium
